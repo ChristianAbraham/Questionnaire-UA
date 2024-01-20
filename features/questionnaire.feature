6e3ade5b6f978e0a3ab1ee3f801d8c992cc52ec3
ChristianAbraham <christian.abraham.putra-2021@fst.unair.ac.id>
2024-01-20 12:49:08 +0700

    first commit

---
+++ features/questionnaire.feature
@@ -0,0 +1,20 @@
+Feature: questionnaire
+    Scenario: Questionnaire
+    Given i am on SignIn Page
+    #change username and password with your username and password
+    When i fill in username field with username 
+    And i fill in password field with password
+    And i press SignIn button
+    Then i should be on Home
+    When i press SIM Kuesioner
+    And i press Mahasiswa S1
+    Then i should be on https://qa.unair.ac.id/qa/kuesioner/dashboard
+    When i press Kinerja Tim Dosen
+    And i press mata kuliah
+    And i press DI PRAK
+    And i fill form with 10
+    And i press dosen
+    And i chose dosen
+    And i press button simpan
+    And i press OK
+    Then i should see Pengisian Kuesioner Berhasil
---
+++ steps/questionnaire.py
@@ -0,0 +1,78 @@
+from behave import *
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+@given("I am on SignIn Page")
+def step_impl(context):
+    context.browser = webdriver.Chrome()
+    context.browser.get("https://qa.unair.ac.id/")
+
+@when("i fill in username field with username")
+def step_impl(context):
+    context.browser.find_element(By.XPATH, '//*[@id="userid"]').send_keys("username")
+
+@when("i fill in password field with password")
+def step_impl(context):
+    context.browser.find_element(By.XPATH, '//*[@id="password"]').send_keys("password")
+
+@when("i press SignIn button")
+def step_impl(context):
+    context.browser.find_element(By.XPATH, '/html/body/div[1]/div/div/div[2]/form/div[4]/button').click()
+
+@Then("i should be on home")
+def step_impl(context):
+    assert "SIM Kuesioner" in context.browser.page_source
+
+@when("i press SIM Kuesioner")
+def step_impl(context):
+    context.browser.find_element(By.XPATH, '//*[@id="navigation"]/li').click()
+
+@when("i press Mahasiswa S1")
+def step_impl(context):
+    context.browser.find_element(By.XPATH, '//*[@id="kuesioner"]/div/div').click()
+
+@then("i should be on https://qa.unair.ac.id/qa/kuesioner/dashboard")
+def step_impl(context):
+    assert "https://qa.unair.ac.id/qa/kuesioner/dashboard" in context.browser.current_url
+
+@when("i press Kinerja Tim Dosen")
+def step_impl(context):
+    context.browser.find_element(By.XPATH, '//html/body/div/div[2]/div[2]/div/div/a[1]/div/div').click()
+
+@when("i press mata kuliah")
+def step_impl(context):
+    context.browser.find_element(By.XPATH, ' //*[@id="idmk"]').click()
+
+@when("i press DI PRAK")
+def step_impl(context):
+    context.browser.find_element(By.XPATH, '//*[@id="idmk"]/option[2]').click()
+
+@when("i fill form with 10")
+def step_impl(context):
+    context.browser.find_element(By.XPATH, '//*[@id="jawaban_715_39_134"]').click()
+    context.browser.find_element(By.XPATH, '//*[@id="jawaban_716_39_134"]').click()
+    context.browser.find_element(By.XPATH, '//*[@id="jawaban_717_39_134"]').click()
+    context.browser.find_element(By.XPATH, '//*[@id="jawaban_718_39_134"]').click()
+    context.browser.find_element(By.XPATH, '//*[@id="jawaban_719_39_134"]').click()
+    context.browser.find_element(By.XPATH, '//*[@id="jawaban_720_39_134"]').click()
+
+@when("i press dosen")
+def step_impl(context):
+    context.browser.find_element(By.XPATH, '//*[@id="dosen"]').click()
+
+@when("i chose dosen")
+def step_impl(context):
+    context.browser.find_element(By.XPATH, '//*[@id="dosen"]/option[2]').click()
+
+@when("i press button simpan")
+def step_impl(context):
+    context.browser.find_element(By.XPATH, '/html/body/div/div[2]/div[2]/div/div/form/div[17]/button').click()
+
+@when("i press OK")
+def step_impl(context):
+    alert = context.browser.switch_to.alert
+    alert.accept()
+
+@then("i should see Pengisian Kuesioner Berhasil")
+def step_impl(context):
+    assert "Pengisian Kuesioner Berhasil" in context.browser.page_source
